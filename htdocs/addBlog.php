ebca9776b99808f3776de322854143cbb459079f
Batuhan Ali Ozturk <154116402+BatuhanAli@users.noreply.github.com>
2024-04-17 16:41:07 +0100

    Add files via upload

--- htdocs/addBlog.php
+++ htdocs/addBlog.php
@@ -1,8 +1,34 @@
 <?php
-$username = $_POST['email'];
-$password = $_POST['password'];
+$title = $_POST['title'];
+$content = $_POST['content'];
+$date = $_POST['date'];
     
-$db_username = "hi@hi";
-$db_password = "hi";
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "portfolio";
 
+$conn = new mysqli($servername, $username, $password, $dbname, 8889);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+if ($conn->query($sql) === TRUE) {
+    $sql = "INSERT INTO blogs (title, date, content) VALUES ('$title', '$date', '$content')";
+
+    $result = $conn->query($sql);
+
+    if ($result === TRUE) {
+        header("Location: Blog.html");
+    } 
+
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        header("Location: addPost.html");
+    }
+
+    $conn->close();
+}
 ?>
